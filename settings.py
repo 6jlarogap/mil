ba60181b4c38677c761497023513faa9dddee641
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2012-01-14 21:52:32 -0500

    mil fix

--- settings.py
+++ settings.py
@@ -17,7 +17,7 @@ SEND_BROKEN_LINK_EMAILS = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'mil',                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
