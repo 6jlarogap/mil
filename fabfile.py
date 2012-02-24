f73bfd92e7ca8e97e56268e6b65a80878117c3e6
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2012-02-23 20:16:49 -0500

    #116

--- fabfile.py
+++ fabfile.py
@@ -2,11 +2,18 @@ from __future__ import with_statement
 from fabric.api import *
 from fabric.contrib.console import confirm
 
-env.hosts = ['youmemory.org']
-
+@hosts(['youmemory.org'])
 def deploy():
     local('git push')
     with cd('/home/www-data/django/mil/'):
         run('sudo -u www-data git pull')
         run('./manage.py migrate')
         run('sudo /etc/init.d/apache2 reload')
+
+    deploy_sev()
+
+@hosts(['sev-test.bsuir.by'])
+def deploy_sev():
+    local('git push')
+    with cd('/opt/mil/contrib/'):
+        run('sudo sh ./update-online.sh')
