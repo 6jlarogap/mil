aea7cffdf27c320f625e24fd0c466ac46f0b3a74
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2012-07-06 20:19:42 -0500

    #159

--- common/tasks.py
+++ common/tasks.py
@@ -3,6 +3,7 @@ from zipfile import ZipFile
 
 from celery.task import task
 from django.conf import settings
+from django.contrib.sites.models import Site
 from django.template.loader import render_to_string
 import os
 from common.models import Burial
@@ -13,21 +14,26 @@ def report_2_deferred(burial_id, email):
     b = Burial.objects.get(pk=burial_id)
     msg = render_to_string('reports/report_2.html', {'burial': b})
 
-    html_path = os.tempnam()
+    html_path = os.tempnam(os.path.join(settings.MEDIA_ROOT, 'reports')) + '.html'
+
+    if not os.path.exists(os.path.dirname(html_path)):
+        os.makedirs(os.path.dirname(html_path))
+
     f = open(html_path, 'w')
     f.write(msg.encode('utf-8'))
     f.close()
 
-    zip_path = os.tempnam()
+    zip_path = html_path + '.zip'
     zip = ZipFile(zip_path, 'w')
     zip.write(html_path, 'report_2.html')
     zip.close()
 
-    email = EmailMessage(subject=u'Форма 2', body=u'См. прикрепленный файл', to=[email], from_email=settings.DEFAULT_FROM_EMAIL)
+    body = u'http://%s%s' % (Site.objects.get_current().domain, html_path.replace(settings.MEDIA_ROOT, settings.MEDIA_URL))
+    email = EmailMessage(subject=u'Форма 2', body=body, to=[email], from_email=settings.DEFAULT_FROM_EMAIL)
     email.attach_file(zip_path)
     email.send()
 
     os.unlink(html_path)
-    os.unlink(zip_path)
+    # os.unlink(zip_path)
 
     return 'ok'
